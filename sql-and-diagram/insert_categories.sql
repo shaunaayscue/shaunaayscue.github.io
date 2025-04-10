cf751536808eadede743f80d0d5e1d6ba0e49df9
Shauna Ayscue <saayscue@uncg.edu>
2025-04-10 11:11:26 -0400

    change

--- sql-and-diagram/insert_categories.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Categories (category_name, priority) VALUES
-('Fiction', 1),
-('Non-Fiction', 2),
-('Romance', 3),
-('Mystery/Thriller', 4),
-('Fantasy', 5);
--- sql-and-diagram/insert_users.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Users (user_name, user_email, user_password, user_type)
-VALUES ('John Doe', 'johndoe@example.com', 'password123', 'shopper');
