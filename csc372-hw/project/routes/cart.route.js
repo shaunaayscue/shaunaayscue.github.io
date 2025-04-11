be0aa0b368b670b70545acb9facf524383b1e51d
Shauna <saayscue@uncg.edu>
2025-04-10 20:30:47 -0400

    Update cart.route.js

--- csc372-hw/project/routes/cart.route.js
+++ csc372-hw/project/routes/cart.route.js
@@ -6,8 +6,8 @@ const cartController = require("../controllers/cart.controller");
 //http://localhost:3000/cart/add
 router.post("/add", cartController.addToCart);
 
-//http://localhost:3000/cart/1/products/12
-router.delete("/:user_id/products/:product_id", cartController.removeProductFromCart);
+//http://localhost:3000/cart/1/products/delete/12
+router.delete("/:user_id/products/delete/:product_id", cartController.removeProductFromCart);
 
 //http://localhost:3000/cart/1/checkout
 router.post("/:user_id/checkout", cartController.checkout);
